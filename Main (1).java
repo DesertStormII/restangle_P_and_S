a3395bbc0517a30b09255bff0b6761a30c7313d8
DesertStormII <119744433+DesertStormII@users.noreply.github.com>
2023-07-02 12:56:25 +0500

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    //restangle
+		int a = 7;
+		int b = 9;
+		int S = a * b;
+		int P = 2 * (a + b);
+		System.out.println(" Perimetr " + P);
+		System.out.println(" Area " + S);
+	}
+}
